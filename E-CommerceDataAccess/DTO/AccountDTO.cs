b5800fe7ea49f0653d9cd488816c9fd9ba9a7fe2
Tareqalnaqeeb <106020192+Tareqalnaqeeb@users.noreply.github.com>
2025-07-08 01:30:18 +0300

    to here we can i say it frist version1

--- E-CommerceDataAccess/DTO/AccountDTO.cs
+++ E-CommerceDataAccess/DTO/AccountDTO.cs
@@ -1,34 +1,47 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.ComponentModel.DataAnnotations;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace E_CommerceDataAccess.DTO
-//{
-//    public class AccountDTO
-//    {
-//    }
-//    public class LoginDTO
-//    {
-//        [Required]
-//        public string UserName { get; set; }
-//        [Required]
-//        public string Password { get; set; }
-//    }
+namespace E_CommerceDataAccess.DTO
+{
+    public class LoginDTO
+    {
+        public string UserName { get; set; }
+public string Password { get; set; }
+    }
 
-//    public class ForgetPasswordDto
-//    {
-//        public string Email { get; set; }
-//    }
-//    public class ResetPasswordDto
-//    {
-//        public string Email { get; set; }
-//        public string Token { get; set; }
-//        public string NewPassword { get; set; }
-//        public string ConfirmNewPassword { get; set; }
-//    }
+    public class ForgetPasswordDto
+{
+    public string Email { get; set; }
+}
+
+public class ResetPasswordDto
+{
+    public string Email { get; set; }
+    public string Token { get; set; }
+    public string NewPassword { get; set; }
+    public string ConfirmNewPassword { get; set; }
+}
 
 
-//}
+public class TokenDTO
+{
+
+
+    public string Email { get; set; }
+
+    public string UserID { get; set; }
+    public string Token { get; set; }
+}
+
+public class VerfiyCodeDto
+{
+    public string Email { get; set; }
+    public string CodeOTP { get; set; }
+}
+
+
+}
--- E-CommerceDataAccess/DTO/UserDTO.cs
+++ E-CommerceDataAccess/DTO/UserDTO.cs
@@ -36,46 +36,6 @@ namespace E_CommerceDataAccess.DTO
         public string Phone { get; set; }
        
     }
-    public class LoginDTO
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-    }
-
-    public class ForgetPasswordDto
-    {
-        public string Email { get; set; }
-    }
-
-    public class ResetPasswordDto
-    {
-        public string Email { get; set; }
-        public string Token { get; set; }
-        public string NewPassword { get; set; }
-        public string ConfirmNewPassword { get; set; }
-    }
-
-    public class AuthResponse
-    {
-        public string Token { get; set; }
-        public string RefreshToken { get; set; }
-        public UserDTO User { get; set; }
-    }
-    public class TokenDTO
-    {
-      
-
-        public string Email { get; set; }
-        
-        public string UserID { get; set; }
-        public string Token { get; set; }
-    }
-
-    public class VerfiyCodeDto
-    {
-        public string Email { get; set; }
-        public string CodeOTP { get; set; }
-    }
-
+ 
 
 }
